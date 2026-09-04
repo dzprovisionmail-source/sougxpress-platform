8b65cdf1ff187c83777576bf042a054ac815cd34
dzprovisionmail-source <2.89212506e+08+dzprovisionmail-source@users.noreply.github.com>
2026-09-04 23:25:27 +0000

    fix(merchant): size store edit modal

--- apps/mobile/src/app/merchant/store.tsx
+++ apps/mobile/src/app/merchant/store.tsx
@@ -973,6 +973,7 @@ const styles = StyleSheet.create({
     borderTopLeftRadius: 20,
     borderTopRightRadius: 20,
     padding: 20,
+    height: "85%",
     maxHeight: "85%",
   },
   modalHeader: {
